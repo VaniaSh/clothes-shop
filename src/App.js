c4e7435658fc4f91210b2d979e9b803a6276d565
Vania Shepetko <iv.ssh.fqu@gmail.com>
2022-02-16 21:43:02 +0200

    adding routing into shop and styling collection-item component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,18 @@ import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import {Route} from "react-router-dom";
 import {Switch} from "react-router";
+import ShopPage from "./pages/shop/shop.componenet";
 
-const Shop = () => {
-    return(
-        <div>
-            <h1>
-                shop
-            </h1>
-        </div>
-    )
-}
 
 function App() {
-  return (
-    <div>
-        <Switch>
-            <Route exact={true} path='/' component={HomePage}/>
-            <Route path='/shop' component={Shop}/>
-        </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact={true} path='/' component={HomePage}/>
+                <Route path='/shop' component={ShopPage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
